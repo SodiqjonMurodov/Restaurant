5927eec7f360ad524db30599a84f0bf38c31ebfa
Sodiqjon <murodovsodiq1800@gmail.com>
2024-10-23 16:29:13 +0500

    fixed bot unlimited messages

--- BACKEND/booking/views.py
+++ BACKEND/booking/views.py
@@ -1,6 +1,5 @@
 import asyncio
 from django.utils import timezone
-
 from rest_framework import generics
 from .models import Booking
 from .serializers import BookingFormSerializer
@@ -25,24 +24,30 @@ class ConfirmReservationAPIView(APIView):
     def get(self, request, token):
         try:
             reservation = Booking.objects.get(confirmation_token=token)
-            reservation.is_confirmed = True
-            reservation.save()
         except Booking.DoesNotExist:
             return Response({'message': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Telegram message
-        created_at_local = timezone.localtime(reservation.created_at)
-        message = (
-            f"Новое поступление бронирование #{reservation.id}.\n"
-            f"Имя клиента: {reservation.full_name}\n"
-            f"Дата и время прибытия: {reservation.day.strftime('%d.%m.%Y')} {reservation.time.strftime('%H:%M')}\n"
-            f"Количество гостей: {reservation.guests}\n"
-            f"Номер телефона: {reservation.phone}\n"
-            f"Создано в дату: {created_at_local.strftime('%d.%m.%Y %H:%M')}\n"
-        )
-        asyncio.run(send_to_telegram(message))
+        if reservation.is_confirmed == False:
+            reservation.is_confirmed = True
+            reservation.save()
+
+            # Telegram message
+            created_at_local = timezone.localtime(reservation.created_at)
+            message = (
+                f"Новое поступление бронирование #{reservation.id}.\n"
+                f"Имя клиента: {reservation.full_name}\n"
+                f"Дата и время прибытия: {reservation.day.strftime('%d.%m.%Y')} {reservation.time.strftime('%H:%M')}\n"
+                f"Количество гостей: {reservation.guests}\n"
+                f"Номер телефона: {reservation.phone}\n"
+                f"Создано в дату: {created_at_local.strftime('%d.%m.%Y %H:%M')}\n"
+            )
+            asyncio.run(send_to_telegram(message))
 
         return Response({'message': 'Booking confirmed'}, status=status.HTTP_200_OK)
+
+
+
+
 
 
 
